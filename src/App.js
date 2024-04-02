42a39a572986fe3148bbc638d114f980b675e640
uasteud <uasteud.guangasig@gmail.com>
2024-04-02 19:58:55 +0200

    update data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import datos from './data/datos.json';
+
 
 function App() {
   const [data, setData] = useState([]);
 
+  useEffect(() => {
+    cargarDatos();
+  }, []);
+
+  /*
   useEffect(() => {
     axios.get('http://localhost:8080/api/data/')
       .then(response => {
@@ -11,8 +18,22 @@ function App() {
       })
       .catch(error => {
         console.error('Error al obtener los datos:', error);
+        console.log('Cargando datos desde el fichero json');
+        setData(datos);
       });
   }, []);
+  */
+  const cargarDatos = () => {
+    axios.get('http://localhost:8080/api/data/')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error al obtener los datos desde el backend:', error);
+        console.log('Cargando datos desde el archivo JSON...');
+        setData(datos);
+      });
+  };
 
   return (
     <div>
