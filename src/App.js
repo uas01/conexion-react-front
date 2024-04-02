2d59a83fc980eb10b483671e43cc15f3a05cc058
uasteud <uasteud.guangasig@gmail.com>
2024-04-02 20:21:06 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import datos from './datos.json';
-
+//import datos from './datos.json';
+import datos from './assets/datos.json';
 
 function App() {
   const [data, setData] = useState([]);
